4849be6f4093051e026fc8447f2f9240e21e2167
Adein <adeinpublic@gmail.com>
2025-12-27 17:01:45 -0500

    Fixed bugs in local nws alert module

--- modules/local_nws_alerts.py
+++ modules/local_nws_alerts.py
@@ -16,7 +16,7 @@ class NwsAlertChecker(BotModule):
         # 1. Get configuration
         zone = self.config.get('zone', "INVALID_ZONE_ID")
 
-        self.logger.info(f"Fetching weather alerts for zone #{zone}...")
+        self.logger.info(f"Fetching weather alerts for zone {zone}...")
 
         # 2. Call the service
         data = self.api_service.get_alerts(zone)
@@ -26,9 +26,10 @@ class NwsAlertChecker(BotModule):
             # No alerts
             self.previous_alert_id = None
             return
-        self.logger.info("NWS Alerts: {data}")
+        self.logger.info(f"NWS Alerts: {data}")
         now_utc = datetime.now(timezone.utc)
-        for alert_to_process in data.reverse():
+        reversed_alerts = data[::-1]
+        for alert_to_process in reversed_alerts:
             if alert_to_process.alert_id == None:
                 # Skip alerts without an ID
                 continue
@@ -38,48 +39,59 @@ class NwsAlertChecker(BotModule):
             if alert_to_process.severity.lower() in ["minor", "unknown"]:
                 # Skip alerts not severe enough
                 continue
-            elif alert_to_process.alert_id == previous_alert_id:
+            elif alert_to_process.alert_id == self.previous_alert_id:
                 # Same alert as before and not expired
                 # Return early
                 return
             else:
                 # Process this alert
-                self_.process_alert(alert_to_process)
+                self._process_alert(alert_to_process)
                 return
         # No valid alerts to process, so clear state data
         self.previous_alert_id = None
 
     def _process_alert(self, alert):
+        self.previous_alert_id = alert.alert_id
         summary_string = "NWS Weather Alert (" + alert.severity + "): " + alert.headline
         area_string = "Areas: " + alert.areas
-        description = alert.description.removeprefix('*').removeprefix(' ').removeprefix("WHAT").removeprefix('.').removeprefix('.').removeprefix('.')
+        description = self._process_description(alert.description)
         remaining_size = 200 - len(summary_string) - 1
         if description != None and len(description) > 3 and remaining_size > 20:
             message_data = TextToSend(
                     summary_string + "\n" + description[:remaining_size],
                     None,
-                    channel_num
+                    self.channel
             )
             self.event_bus.publish("meshtastic_service.to_send", message_data)
         elif len(summary_string) + len(area_string) <= 199:
             message_data = TextToSend(
                     summary_string + "\n" + area_string,
                     None,
-                    channel_num
+                    self.channel
             )
             self.event_bus.publish("meshtastic_service.to_send", message_data)
         elif len(summary_string) <= 200:
             message_data = TextToSend(
                     summary_string,
                     None,
-                    channel_num
+                    self.channel
             )
             self.event_bus.publish("meshtastic_service.to_send", message_data)
         else:
             message_data = TextToSend(
                     summary_string[:200],
                     None,
-                    channel_num
+                    self.channel
             )
             self.event_bus.publish("meshtastic_service.to_send", message_data)
+
+    def _process_description(self, description):
+        new_description = description.replace("* WHAT...", "")
+        new_description = new_description.replace("* WHERE...", "")
+        new_description = new_description.replace("* WHEN...", "")
+        new_description = new_description.replace("* IMPACTS...", "")
+        new_description = new_description.replace("* ADDITIONAL DETAILS...", "")
+        new_description = new_description.replace("\n\n", "\n")
+        return new_description
+
 
